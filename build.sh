cd99026c04117929a392b2406a43e3c7dcc26c71
castellabakery <castellabakery@gmail.com>
2023-11-11 17:54:32 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ imageName="jdk17test"
 tarName="jdk17test.tar"
 buildPath="/build/jdk17test"
 
-sudo docker login --username admin --password 'dlwk2452!@' registry.pastelcloud.store
+#sudo docker login --username admin --password 'dlwk2452!@' registry.pastelcloud.store
 sudo docker build -t $registry/$imageName:$version $buildPath
-sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
-sudo ctr i import $buildPath/$tarName
-sudo ctr i push $registry/$imageName:$version
+sudo docker push $registry/$imageName:$version
+#sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
+#sudo ctr i import $buildPath/$tarName
+#sudo ctr i push $registry/$imageName:$version
