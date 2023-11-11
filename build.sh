dcbd9282f1b39b6c3eb6cbf21c972ce5bea4ffcf
castellabakery <castellabakery@gmail.com>
2023-11-11 17:00:23 +0900

    change image registry info

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 version="0.0.2"
-registry="localhost:32500"
+registry="registry.pastelcloud.store"
 imageName="jdk17test"
 tarName="jdk17test.tar"
 buildPath="/build/jdk17test"
 
+sudo docker login --username admin --password 'dlwk2452!@' registry.pastelcloud.store
 sudo docker build -t $registry/$imageName:$version $buildPath
 sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
 sudo ctr i import $buildPath/$tarName
