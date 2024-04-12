ab563a9150794f90b50cb279c5ae226f28cccc76
castelbaker <castellabakery@gmail.com>
2024-04-12 16:37:34 +0900

    도메인 변경 - store -> shop

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 version="0.0.2"
-registry="registry.pastelcloud.store"
+registry="registry.pastelcloud.shop"
 imageName="jdk17test"
 tarName="jdk17test.tar"
 buildPath="/build/jdk17test"
 
-#sudo docker login --username admin --password 'dlwk2452!@' registry.pastelcloud.store
+#sudo docker login --username admin --password 'dlwk2452!@' registry.pastelcloud.shop
 sudo docker build -t $registry/$imageName:$version $buildPath
 sudo docker push $registry/$imageName:$version
 #sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
